e5ca3b64c3244322177c29a293f0895a4ff613f8
Kaankesk <kaankeskin1994@gmail.com>
2022-05-19 13:52:39 +0300

    19.5.2022 commit

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //Loosely Coupled
+        //IoC Container -- Inversion of Control
+        private IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            //Dependency chain
+            var result = _productService.GetAll();
+            return result.Data;
+        }
+
+    }
+}
